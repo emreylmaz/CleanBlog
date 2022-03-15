60e9662146931e328f81d879c39d27db5d59a20f
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-15 14:32:35 +0300

    create project.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const blog = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog description',
+  };
+
+  res.send(blog);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running port: ${port}`);
+});
