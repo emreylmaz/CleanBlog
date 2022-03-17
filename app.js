b138db5aa18fd0af02b21a6b4acb5d2e749cfe04
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-18 02:34:46 +0300

    18.03.2022-T02:34AM  Update

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const Post = require('./models/Post');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
-  res.render('index');
+app.get('/', async (req, res) => {
+  const post = await Post.find({});
+  res.render('index', {
+    post,
+  });
 });
 app.get('/about', (req, res) => {
   /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
@@ -20,7 +34,10 @@ app.get('/add', (req, res) => {
   /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
   res.render('add');
 });
-
+app.post('/post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running port: ${port}`);
